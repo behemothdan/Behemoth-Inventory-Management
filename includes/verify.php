824b964456396af95a5536f4c955584046d8e43f
behemothdan <gray.daniel@gmail.com>
2017-12-29 11:56:59 -0700

    Fixed login issues

--- includes/verify.php
+++ includes/verify.php
@@ -5,21 +5,22 @@
 	$tbl_name="users";
 
 	$myemail=$_POST['email']; 
-	$mypassword=$_POST['password']; 
+	$mypassword=$_POST['password'];	
 
 	// To protect MySQL injection
 	$myemail = stripslashes($myemail);
 	$mypassword = stripslashes($mypassword);
 	$encryptedpassword = md5($mypassword);
-	$sql="SELECT * FROM $tbl_name WHERE email='$myemail' and password='$encryptedpassword'";	
-	
-	$count = $db->query($sql);
+	$sql="SELECT * FROM $tbl_name WHERE email='$myemail' and password='$encryptedpassword'";
+	$count = $db->prepare($sql);
+	$count->execute();
+	$count = $count->fetchAll();
 
 	session_start();
 
-	if($count==1){    
+	if($count[0][0]==1){
 		$_SESSION["myemail"] = $myemail;
-		$_SESSION["encryptedpassword"] = $encryptedpassword; 
+		$_SESSION["encryptedpassword"] = $encryptedpassword; 		
 		header("location:../dashboard/");
 	}
 	else {
